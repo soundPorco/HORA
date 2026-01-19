81cb502aa93224337017b2373e1868ca2c48aaa2
水上京三 <porco.rosso016@gmail.com>
2026-01-19 13:44:18 +0900

    build: deploy to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
     plugins: [react()],
     base: "/HORA/", // ← リポジトリ名
+    build: {
+        outDir: "docs",
+    },
 });
