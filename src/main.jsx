1b9b9f124b633b4f2c3c4f56aa53902db9650ba1
水上京三 <porco.rosso016@gmail.com>
2026-01-19 13:34:44 +0900

    fix: main.jsxを編集　関連　Issue #14

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-// ...existing code...
 import "./index.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
+
 import App from "./App.jsx";
-import Create from "./pages/Create.jsx"; // または ./pages/create.jsx（実ファイル名に合わせる）
+import Create from "./pages/Create.jsx";
 import CreateNew from "./pages/CreateNew.jsx";
 import CreateList from "./pages/CreateList.jsx";
 import Answer from "./pages/Answer.jsx";
@@ -16,17 +16,20 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <StrictMode>
         <HashRouter>
             <Routes>
+                {/* トップ */}
                 <Route path="/" element={<App />} />
+
                 {/* 共通レイアウト */}
-                <Route path="/" element={<EditLayout />}>
-                    <Route path="/edit/:formId" element={<Create />} />
-                    <Route path="/preview/:formId" element={<Preview />} />
-                    <Route path="/result/:formId" element={<Result />} />
+                <Route element={<EditLayout />}>
+                    <Route path="edit/:formId" element={<Create />} />
+                    <Route path="preview/:formId" element={<Preview />} />
+                    <Route path="result/:formId" element={<Result />} />
                 </Route>
-                {/* 新規作成 */}
-                <Route path="/create-new" element={<CreateNew />} />
-                <Route path="/answer/:formId" element={<Answer />} />
-                <Route path="/create-list" element={<CreateList />} />
+
+                {/* 単独ページ */}
+                <Route path="create-new" element={<CreateNew />} />
+                <Route path="answer/:formId" element={<Answer />} />
+                <Route path="create-list" element={<CreateList />} />
             </Routes>
         </HashRouter>
     </StrictMode>
