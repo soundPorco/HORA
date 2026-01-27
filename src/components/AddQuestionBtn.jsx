a5d2a05101709fa8a8f0ea5bc0d18accdbf1f4a9
水上京三 <porco.rosso016@gmail.com>
2026-01-27 15:53:11 +0900

    update: AddQuestionBtnのデザインをシンプルに変更。

--- src/components/AddQuestionBtn.jsx
+++ src/components/AddQuestionBtn.jsx
@@ -1,11 +1,14 @@
 // src/components/AddQuestionBtn.jsx
+
+import { MdAddCircleOutline } from "react-icons/md";
+
 const AddQuestionBtn = ({ addQuestion }) => {
     return (
         <button
             onClick={addQuestion}
-            className="mt-4 w-full py-2 bg-blue-500 text-white rounded hover:bg-blue-600 duration-200"
+            className="flex items-center justify-center mt-4 w-[150px] py-2 gap-1 bg-gray-400 text-white rounded-full duration-200 m-auto hover:bg-gray-500"
         >
-            ＋ 設問を追加
+            <MdAddCircleOutline /> 設問を追加
         </button>
     );
 };
